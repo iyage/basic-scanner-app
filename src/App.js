d30d98cf91e1908a2af511ca66665acb97125314
iyage <yahg.concept@gmail.com>
2023-02-22 22:01:30 +0100

    adjustment made

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import { Route, Routes} from 'react-router-dom';
 import Container from './pages/Container';
 import NotFound from './pages/NotFound';
