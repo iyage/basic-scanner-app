985fe797bb2a8d9a4157cfcaa2c6908465c4568d
iyage <yahg.concept@gmail.com>
2023-02-23 23:02:14 +0100

    testing if app wold work

--- src/pages/Commission.jsx
+++ src/pages/Commission.jsx
@@ -1,6 +1,6 @@
 import { IconButton } from '@material-ui/core'
-import React, { useRef } from 'react'
-import {Html5QrcodeScanner} from "html5-qrcode"
+// import React, { useRef } from 'react'
+import {Html5Qrcode} from "html5-qrcode"
 import styled from 'styled-components';
 import { FaCamera } from 'react-icons/fa';
 
@@ -14,30 +14,43 @@ const Container = styled.div`
   width: 98%;
 `
 function Commission() {
-  function onScanSuccess(decodedText, decodedResult) {
-  // handle the scanned code as you like, for example:
-  alert(`Code matched = ${decodedText}`, decodedResult);
-}
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
-const reader = useRef()
+//   function onScanSuccess(decodedText, decodedResult) {
+//   // handle the scanned code as you like, for example:
+//   alert(`Code matched = ${decodedText}`, decodedResult);
+// }
+// function onScanFailure(error) {
+//   // handle scan failure, usually better to ignore and keep scanning.
+//   // for example:
+//   console.warn(`Code scan error = ${error}`);
+// }
+// const reader = useRef()
 
 // html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
   return (
     <Container>
-      <div id='reader' ref={reader} width='90%'></div>
+      <div id='reader'  width='90%'></div>
       <ScannerContainer style={{height:'60vh'}}>
       <IconButton
         onClick={()=>{
-          let html5Qrcoder= new Html5QrcodeScanner(
-  "reader",
-  { fps: 10, qrbox: {width: 250, height: 250} },
-  /* verbose= */ false);
-        html5Qrcoder.start({ facingMode: "environment" },onScanSuccess,onScanFailure);
+          
+          // eslint-disable-next-line no-unused-vars
+          let  lastResult,countResults=0;
+            const html5QrCode = new Html5Qrcode("reader");
+            const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            /* handle success */
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                alert(decodedText)
+                // gtin = decodedText.trim();
+                // gtin = gtin.replace(/[^a-z0-9]/gi, '');
+                // fetchDetails(gtin);
+                html5QrCode.stop();
+            }
+        };
+        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
   }}
       
       >
