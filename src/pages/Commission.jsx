23e16609b80f3c554b0e1b5014fb4c10c41a178e
iyage <yahg.concept@gmail.com>
2023-02-23 22:14:49 +0100

    testing if app wold work

--- src/pages/Commission.jsx
+++ src/pages/Commission.jsx
@@ -1,45 +1,61 @@
-// import { IconButton } from '@material-ui/core'
-import React, {  useEffect} from 'react'
-import {Html5QrcodeScanner} from "html5-qrcode"
-// import styled from 'styled-components';
-// import { FaCamera } from 'react-icons/fa';
+import { IconButton } from '@material-ui/core'
+import React, {  useState} from 'react'
+import {Html5Qrcode} from "html5-qrcode"
+import styled from 'styled-components';
+import { FaCamera } from 'react-icons/fa';
 
-// const ScannerContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
+const ScannerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-// `
-// const Container = styled.div`
-//   width: 98%;
-// `
-
-
-
-
-
-
+`
+const Container = styled.div`
+  width: 98%;
+`
 function Commission() {
 
-  useEffect(()=>{
-    function onScanSuccess(decodedText, decodedResult) {
-  // handle the scanned code as you like, for example:
-  alert(`Code matched = ${decodedText}`, decodedResult);
-}
+//   useEffect(()=>{
+//     function onScanSuccess(decodedText, decodedResult) {
+//   // handle the scanned code as you like, for example:
+//   alert(`Code matched = ${decodedText}`, decodedResult);
+// }
 
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
+// function onScanFailure(error) {
+//   // handle scan failure, usually better to ignore and keep scanning.
+//   // for example:
+//   console.warn(`Code scan error = ${error}`);
+// }
 
-let html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader",
-  { fps: 10, qrbox: {width: 250, height: 250} },
-  /* verbose= */ false);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+// let html5QrcodeScanner = new Html5QrcodeScanner(
+//   "reader",
+//   { fps: 10, qrbox: {width: 250, height: 250} },
+//   /* verbose= */ false);
+// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
-  },[])
+//   },[])
+
+const  [lastResult,setLastResult]= useState(0);
+const  [countResults,setCountResults]= useState(0);
+
+    function scannManually() {
+        const html5QrCode = new Html5Qrcode("reader");
+        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        /* handle success */
+        if (decodedText !== lastResult) {
+          // eslint-disable-next-line no-const-assign
+          setCountResults(++countResults);
+          setLastResult(decodedText)
+            // lastResult = ;
+            // gtin = decodedText.trim();
+            // gtin = gtin.replace(/[^a-z0-9]/gi, '');
+            html5QrCode.stop();
+        }
+    };
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+        
+    }
 
 
 //   async function runScan ()
@@ -80,66 +96,27 @@ html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 // const video = useRef();
 // const [scannerState,setScannerState] = useState(false)
   return (
-    <div id='reader' width="400px" style={{color:'red'}}></div>
-  )}
-
-    // <div className="App">
-    //   <h1>Hello CodeSandbox</h1>
-    //   <h2>
-    //     Last Scan:
-    //     {selected}
-    //   </h2>
-    //   <button
-    //     onClick={() => {
-    //       setStartScan(!startScan);
-    //     }}
-    //   >
-    //     {startScan ? "Stop Scan" : "Start Scan"}
-    //   </button>
-    //   {startScan && (
-    //     <>
-    //       <select onChange={(e) => setSelected(e.target.value)}>
-    //         <option value={"environment"}>Back Camera</option>
-    //         <option value={"user"}>Front Camera</option>
-    //       </select>
-    //       <QrReader
-    //         facingMode={selected}
-    //         delay={1000}
-    //         onError={handleError}
-    //         onScan={handleScan}
-    //         // chooseDeviceId={()=>selected}
-    //         style={{ width: "300px" }}
-    //       />
-    //     </>
-    //   )}
-    //   {loadingScan && <p>Loading</p>}
-    //   {data !== "" && <p>{data}</p>}
-    // </div>
-
-//     <Container>
-//      {!scannerState&& <ScannerContainer style={{height:'60vh'}}>
-//       <IconButton
-//         onClick={()=>{
-//     setScannerState(true);
-//      runScan ()
-
-//                    }}
+    <Container>
+      <ScannerContainer style={{height:'60vh'}}>
+      <IconButton
+        onClick={()=>{
+    // setScannerState(true);
+    scannManually();}}
       
-//       >
-//   <FaCamera size={60}
-//   />
-// </IconButton>
-//       </ScannerContainer>
-// }
+      >
+  <FaCamera size={60}
+  />
+</IconButton>
+      </ScannerContainer>
 
-// {   scannerState&& <ScannerContainer>
-//      <video  ref={video} id="video" width="97%" height="250" style={{border: "1px solid red"}}></video>
-//     </ScannerContainer>}
+{/* {   scannerState&& <ScannerContainer> */}
+     {/* <video  ref={video} id="video" width="97%" height="250" style={{border: "1px solid red"}}></video> */}
+    {/* </ScannerContainer>} */}
 
-//     </Container>
+    </Container>
 
 
-  // )
-// }
+  )
+}
 
 export default Commission
